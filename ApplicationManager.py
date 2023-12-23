911dad4c16dfb1e69aa9838df73f5af3c6433cd6
Omar-Khaled-19 <omarkhaled191104@gmail.com>
2023-12-23 14:31:54 +0200

    Spectrogram is plotted without saving file

--- ApplicationManager.py
+++ ApplicationManager.py
@@ -1,6 +1,5 @@
-import sounddevice as sd
-import soundfile as sf
-import librosa as lb
+import pyaudio
+import numpy as np
 from PyQt5.QtGui import QPixmap
 
 
@@ -8,10 +7,12 @@ class ApplicationManger:
     def __init__(self, ui):
         self.ui = ui
         self.fingerprint_mode = False
-        self.recorded_voice_text = ""
-        self.recorded_voice = None
+        self.audio_input = pyaudio.PyAudio()
+        self.recorded_data = None
+        
         self.pass_sentences = ["grant me access", "open middle door", "unlock the gate"]
         self.pass_sentences_progress_bars = [ui.grant_progressBar, ui.open_progressBar, ui.unlock_progressBar]
+        
         self.right_mark_icon = QPixmap("Assets/Correct.png")
         self.right_mark_icon = self.right_mark_icon.scaledToWidth(50)
         self.wrong_mark_icon = QPixmap("Assets/Wrong.png")
@@ -21,15 +22,28 @@ class ApplicationManger:
         self.fingerprint_mode = not self.fingerprint_mode 
     
     def record_voice(self):
-        if self.ui.Public_RadioButton.isChecked:
-            duration = 3  # seconds
-            sampling_frequency = 44100
-            self.recorded_voice = sd.rec(frames=int(sampling_frequency*duration), samplerate=sampling_frequency,
-                                         channels=1, blocking=True, dtype='int16')
-            sf.write("output.wav", self.recorded_voice, sampling_frequency)
-            self.recorded_voice, sampling_frequency = lb.load("output.wav")
-            self.ui.SpectrogramWidget.canvas.plot_spectrogram(self.recorded_voice, sampling_frequency)
-            print(self.recorded_voice)
+        sample_rate = 44100
+        chunk = 1024
+        duration = 3
+
+        self.stream = self.audio_input.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=44100,
+            input=True,
+            frames_per_buffer=1024
+        )
+
+        print("Recording...")
+
+        self.recorded_data = []
+        for i in range(0, int(sample_rate / chunk * duration)):
+            data = np.frombuffer(self.stream.read(chunk), dtype=np.int16)
+            self.recorded_data.append(data)
+
+        print("Finished recording.")
+        self.ui.SpectrogramWidget.canvas.plot_spectrogram(self.recorded_data, 44100)
+
 
     def display_text(self):
         self.ui.VoiceRecognizedLabel.setText(self.recorded_voice_text)
